d1c33bf6d00347ad0eb27171c663bf54028487ff
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-11-13 20:12:30 +0700

    Zip file check

--- mc2mt.py
+++ mc2mt.py
@@ -1,7 +1,13 @@
+import zipfile
 from argparse import ArgumentParser, Namespace
+from os import path
 
 
 def main(args: Namespace) -> int:
+    if not zipfile.is_zipfile(args.input):
+        print("Input is not a zip file")
+        return 1
+
     return 0
 
 
@@ -10,5 +16,8 @@ if __name__ == "__main__":
         prog="mc2mt",
         description="Convert Minecraft resource packs to Minetest texture packs",
     )
+
+    parser.add_argument("input", help="Input resource pack")
+    parser.add_argument("-o", "--output", help="Output texture pack")
 
     exit(main(parser.parse_args()))
